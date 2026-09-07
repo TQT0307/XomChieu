7f6efb211afd99be5e438efd119beae5a6914096
TQT0307 <tranquocthien3703@gmail.com>
2026-09-08 00:31:50 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1013,8 +1013,8 @@ export default function App() {
             activeNavSection={activeNavSection}
             setActiveNavSection={setActiveNavSection}
           />
-         {adminSessionStatus === 'guest' && <VisitorNamePrompt />}
-</PublicErrorBoundary>
+          <VisitorNamePrompt disabled={adminSessionStatus !== 'guest'} />
+          </PublicErrorBoundary>
         )}
       </main>
 
